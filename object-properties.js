38cc1d62a780dfa408761834ffd6aec56c50b42c
PaolaP <paolaperez8@gmail.com>
2021-09-28 02:45:59 +0000

    success

---
+++ object-properties.js
@@ -0,0 +1,16 @@
+const example = {
+  pizza: 'yummy'
+}
+
+console.log(example['pizza']);
+
+example.pizza
+
+example['pizza']
+
+// challenge
+const food = {
+  types: 'only pizza'
+}
+console.log(food['types']);
+food.types;
